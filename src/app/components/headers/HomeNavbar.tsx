dd3dbe86c6ef389f9d03c372f8890bdd564fbfe5
Tony <diyorbeksolijonov27@gmail.com>
2025-08-16 18:20:02 +0900

    feat: practise useState & useEffect

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,9 +1,27 @@
 import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Basket from "./Basket";
+import React,{ useEffect, useState } from "react";
 
 export default function HomeNavbar() {
-   const authMember = true;
+   const authMember = null;
+   const [count, setCount] = useState<number>(0);
+   const [value, setValue] = useState<boolean>(true);
+
+   useEffect(()=>{
+      console.log("componentDidMount",count); //DATA FETCH
+        setCount(count+1);
+
+        return()=>{
+         console.log("componentWillUnmount")
+        }
+   },[value])
+
+
+// Handlers
+   const buttonHandler = ()=>{
+      setValue(!value);
+   }
 
    return (<div className="home-navbar">
        <Container className="navbar-container"> 
@@ -62,11 +80,13 @@ export default function HomeNavbar() {
                      World's Most Delicious Cousine
                   </Box>
                   <Box className={"wel-txt"}> The Choice, not just a choice</Box>
-                  <Box className={"service-txt"}>24 hours service</Box>
+                  <Box className={"service-txt"}>{count} hours service</Box>
                   <Box className={"signup"}>
                      {!authMember ? (
                         <Button 
-                        variant={"contained"} className={"signup-button"}>
+                        variant={"contained"} className={"signup-button"}
+                        // onClick={()=> setCount(count+1)}
+                        onClick={buttonHandler}>
                            SIGN UP
                         </Button>
                      ) : null}
